c494aea6bd968fa59165e7ee81a807d2ceaad984
goforbroke <go.for.broke1006@gmail.com>
2022-11-26 14:49:07 +0200

    Add setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import setuptools
 
 setup(name='gfb-kafka-tools',
-      version='1.0',
+      version='v1.0.0',
       description='GFB Kafka Tools',
       author='GFB',
       author_email='go.for.broke1006@python.net',
